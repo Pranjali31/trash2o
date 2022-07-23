b9b8ac0d2b485f5885cdc15c8964b4325374c43b
Karan Zaveri <48907668+karanzaveri5@users.noreply.github.com>
2022-07-23 18:37:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <View>
+      <Text>About Us</Text>
+      <Text style={}  > //formating
+              Tras.h20 is a one stop solution for getting to know the refill stations that are avaible nearby as well as recording the data of the usage and frequency of the refilling ,For each activity perfomred few points are added into the users account which could be used to redeem for a coupon from the ones that are availble. 
+            </Text>
+    </View>
+  );
+};
+
+export default AboutUs;
