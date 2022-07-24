faffc78f610de50d98def800b4e7ace7c46b2421
Karan Zaveri <48907668+karanzaveri5@users.noreply.github.com>
2022-07-24 13:05:56 -0400

    Add files via upload

--- Frontend/src/screens/aboutUs/index.js
+++ Frontend/src/screens/aboutUs/index.js
@@ -6,7 +6,18 @@ const AboutUs = () => {
     <View>
       <Text>About Us</Text>
       <Text style={}  > //formating
-              Tras.h20 is a one stop solution for getting to know the refill stations that are avaible nearby as well as recording the data of the usage and frequency of the refilling ,For each activity perfomred few points are added into the users account which could be used to redeem for a coupon from the ones that are availble. 
+      Tras.h20 Tras.h20 is a one stop solution for getting to know the refill stations that are avaible nearby as well as recording the data of the usage and frequency of the refilling ,For each activity perfomred few points are added into the users account which could be used to redeem for a coupon from the ones that are availble. 
+
+      Our Mission 
+      Tras.h20’s objective is to provide a platform and service that could contribute to the reusability of containers as well as motivate users to go green as well as encourage others to join in.
+      Get to Know our team:
+      Ayush Kaushik Mistry 
+      Harsh Bharath Nimmakuri 
+      Karan Amitkumar Zaveri 
+      Marc Smith 
+      Parth Ashokbhai Patel 
+      Pranjali Mukeshkumar Prajapati 
+
             </Text>
     </View>
   );
